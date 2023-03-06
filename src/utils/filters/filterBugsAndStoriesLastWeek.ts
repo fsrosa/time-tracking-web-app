aab62c66415e4fb5f775f81da81faae99433e1b3
frosa <fabriciosilvarosa@gmail.com>
2023-03-06 01:16:56 -0300

    feat: filter abstractions

---
+++ src/utils/filters/filterBugsAndStoriesLastWeek.ts
@@ -0,0 +1,32 @@
+import { Task } from '@/models/task'
+import moment from 'moment'
+
+const filterBugsAndStoriesLastWeek = (tasks: Task[], type: "BUG" | "STORY") => {
+  const tasksLastWeek = tasks.filter((task) => {
+    const endDate = moment(task.endDate, 'YYYY-MM-DD HH:mm:ss');
+    const lastWeek = moment().subtract(1, 'weeks').startOf('week');
+    const endOfLastWeek = moment().subtract(1, 'weeks').endOf('week');
+    return (
+      task.type === type &&
+      endDate.isBetween(lastWeek, endOfLastWeek, undefined, '[]')
+    );
+  });
+  const tasksLastWeekCount = tasksLastWeek.length;
+
+  const tasksPreviousWeek = tasks.filter((task) => {
+    const endDate = moment(task.endDate, 'YYYY-MM-DD HH:mm:ss');
+    const previousWeek = moment().subtract(2, 'weeks').startOf('week');
+    const endOfPreviousWeek = moment().subtract(2, 'weeks').endOf('week');
+    return (
+      task.type === type &&
+      endDate.isBetween(previousWeek, endOfPreviousWeek, undefined, '[]')
+    );
+  });
+  const tasksPreviousWeekCount = tasksPreviousWeek.length;
+
+  const percentDifference = ((tasksLastWeekCount - tasksPreviousWeekCount) / tasksPreviousWeekCount) * 100;
+
+  return { tasksLastWeekCount, tasksPreviousWeekCount, percentDifference };
+}
+
+export default filterBugsAndStoriesLastWeek
