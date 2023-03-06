17cb1b45e806a5d412826c52bbe23e3ea8396673
frosa <fabriciosilvarosa@gmail.com>
2023-03-06 01:14:05 -0300

    fix: optional values to Task

--- src/models/task.ts
+++ src/models/task.ts
@@ -1,11 +1,11 @@
 export type Task = {
-    id: string;
-    name: string;
-    type: string;
-    timeSpent: number;
-    startDate: string;
-    endDate: string;
-    labels: string[];
+    id?: string;
+    name?: string;
+    type?: string;
+    timeSpent?: number;
+    startDate?: string;
+    endDate?: string;
+    labels?: string[];
 }
 
 var tasks: Array<Task> = []
