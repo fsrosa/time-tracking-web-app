56cebc38cff7431c4b07f462002be90cc070bbaa
frosa <fabriciosilvarosa@gmail.com>
2023-03-06 01:16:11 -0300

    feat: new endpoints and localhost abstraction

--- src/service/apiClient.ts
+++ src/service/apiClient.ts
@@ -1,7 +1,22 @@
 import useAxios from 'axios-hooks'
 
-const getUrl = (path = '') => `https://${process.env.VERCEL_URL}/api${path}`
+let apiDomain: string;
+
+if(process.env.VERCEL_URL)
+    apiDomain = `https://${process.env.VERCEL_URL}`
+else
+    apiDomain = 'http://localhost:3000'
+
+const getUrl = (path = '') => `${apiDomain}/api${path}`
 
 export const useTask = () => useAxios({
     url: getUrl('/task'),
+}, { manual: true })
+
+export const useTaskId = (id: any) => useAxios({
+    url: getUrl(`/task/${id}`),
+}, { manual: true })
+
+export const useOverview = () => useAxios({
+    url: getUrl('/overview')
 }, { manual: true })
